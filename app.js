e5c143c2703585b6337e36c2c24b59417c3dbd18
Javed Khan <javed.khan@u-next.com>
2022-12-04 22:48:51 +0530

    add chat

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
 const express= require('express')
 const cors = require('cors');
 const app= express();
+const http = require("http");
+const server = http.createServer(app);
 const corsOptions = {
 	origin: 'http://localhost:3000',
 	optionsSuccessStatus: 200
 };
+
+
 app.use(cors(corsOptions));
+
+const socketIO = require("socket.io");
+const io = socketIO(server);
+const users = [{}];
+
+io.on("connection", (socket)=>{
+
+    console.log("sssssssssss",socket);
+
+    socket.on("joined",({user})=>{
+        users[socket.id] = user;
+        console.log(`${user} has joined.`);
+        socket.broadcast.emit('userJoined',{user:"Admin", message:`${users[socket.id]} User has joined`});
+        socket.emit('welcome', {user:"Admin", message:`welcome to the chat, ${users[socket.id]}`})
+    });
+    socket.on('message',({message,id})=>{
+        io.emit('sendMessage',{user:users[id], message,id});
+    })
+    socket.on('disconnect',()=>{
+        socket.broadcast.emit('leave', {user:"Admin", message:`User has left`});
+        console.log(`User Left, ${users[socket.id]} `);
+    })
+
+
+})
+
+
+
 const userRouter= require('./router/userRouter')
 const adminRouter= require('./router/adminRouter')
 const registerRouter= require('./router/registerRouter')
