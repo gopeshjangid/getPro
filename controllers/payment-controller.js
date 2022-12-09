2639bba3127217310298dcb1f7003619565d9a0f
bablu <bablusaini90310@gmail.com>
2022-12-09 12:05:27 +0530

    change payment api

--- controllers/payment-controller.js
+++ controllers/payment-controller.js
@@ -1,29 +1,31 @@
-const Stripe=require("stripe")
-const dotenv = require('dotenv');
+const Stripe = require("stripe");
+const dotenv = require("dotenv");
 dotenv.config();
-const stripe= Stripe(process.env.SECRET)
+const stripe = Stripe(process.env.SECRET);
 
-module.exports.payment = async(req,res)=>{
-   const wallet=req.body.wallet
-   try {
-    const session= await stripe.checkout.sessions.create({
-      line_items: [{
+module.exports.payment = async (req, res) => {
+  const wallet = req.body.wallet;
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
           price_data: {
-            currency: 'inr',
+            currency: "inr",
             product_data: {
-              name: 'Wallet',
+              name: "Wallet",
             },
-            unit_amount: wallet*100,
+            unit_amount: wallet * 100,
           },
           quantity: 1,
-        }],
-          mode:'payment',
-          success_url:'https://example.com/success',
-          cancel_url:'https://example.com/cancel',
-     });
-     res.redirect(303,session.url)
-   } catch (error) {
-    res.send(error)
-   }
-   
-    }
+        },
+      ],
+      mode: "payment",
+      success_url: "http://localhost:3000/success",
+      cancel_url: "http://localhost:3000/fail",
+    });
+    res.send(200, { url: session.url, id: session.id });
+    console.log(session)
+  } catch (error) {
+    res.send(error);
+  }
+};
