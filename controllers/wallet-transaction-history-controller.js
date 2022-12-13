4d66b0a18adee5c1abe5524875515d9f5990d2ba
bablu <bablusaini90310@gmail.com>
2022-12-13 12:12:31 +0530

    impliment credit total and debit total

--- controllers/wallet-transaction-history-controller.js
+++ controllers/wallet-transaction-history-controller.js
@@ -10,9 +10,31 @@ module.exports.getWalletTransactionHistory = async (req, res) => {
         const UserDetails = await User.findById(verifyTokenId.userId);
         const creditHistory = await Wallet.find({ $and: [{ user: UserDetails.email }, { pay_type: "credited" }] })
         const debitHistory = await Wallet.find({ $and: [{ user: UserDetails.email }, { pay_type: "debited" }] })
+        let totalCredit = 0
+        let totalDebit = 0
+        if (creditHistory.length < 1) {
+            res.status(200).json({
+                message: []
+            })
+        } else {
+            for (var i = 0; i < creditHistory.length; i++) {
+                totalCredit += creditHistory[i].wallet
+            }
+        };
+         if (debitHistory.length < 1) {
+            res.status(200).json({
+                message: []
+            })
+        } else {
+            for (var i = 0; i < debitHistory.length; i++) {
+                totalDebit += debitHistory[i].wallet
+            }
+        }
         res.status(200).json({
             credit: creditHistory,
-            debit: debitHistory
+            totalCredit: totalCredit,
+            debit: debitHistory,
+            totalDebit:totalDebit
         })
     } catch (error) {
         res.status(500).json({
