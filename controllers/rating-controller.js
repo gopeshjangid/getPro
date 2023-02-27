46e56d5048c4113fd75a5193845f5320f8d58087
bablu <bablusaini90310@gmail.com>
2023-02-27 11:00:34 +0530

    impiment review functionality

--- controllers/rating-controller.js
+++ controllers/rating-controller.js
@@ -1,7 +1,15 @@
+const Rating= require("../model/rating")
+
 module.exports.rating = async (req, res) => {
     try {
-       console.log("hiiiii")
-       
+      const username=req.body.username
+      const email=req.body.email
+      const title=req.body.title
+      const description=req.body.description
+      const rating=req.body.rating
+      const ratingData=  new Rating({username:username,email:email,title:title,description:description,rating:rating,status:"unread"})
+      await  ratingData.save()
+      res.status(200).json({message:"review submited"})
     } catch (error) {
         res.status(500).json({
             error: error.message
