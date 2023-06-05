3d189699ae602a2e366741fc29e3c9e590f1e9d1
Prabhat Nagpal <nagpal.prabhat.prabhat@gmail.com>
2023-06-05 10:40:04 +0530

    Removed throw error in save-payment-method-controller.js

--- controllers/save-payment-method-controller.js
+++ controllers/save-payment-method-controller.js
@@ -42,7 +42,6 @@ module.exports.FetchPaymentMethod = async (req, res) => {
     res.status(200).json({ message: PaymentDetails });
   } catch (error) {
     res.status(400);
-    throw new Error(error.message);
   }
 };
 
